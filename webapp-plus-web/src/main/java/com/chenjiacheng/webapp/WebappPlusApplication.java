3dc8c2759e775a1afe938a191dc3d7dc068d5626
chenjiacheng <chen-jiacheng@foxmail.com>
2024-12-23 00:12:15 +0800

    增加定时任务&线程池配置

--- webapp-plus-web/src/main/java/com/chenjiacheng/webapp/WebappPlusApplication.java
+++ webapp-plus-web/src/main/java/com/chenjiacheng/webapp/WebappPlusApplication.java
@@ -3,6 +3,7 @@ package com.chenjiacheng.webapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by chenjiacheng on 2024/11/25 23:53
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class WebappPlusApplication {
 
--- webapp-plus-web/src/main/java/com/chenjiacheng/webapp/startup/ApplicationStartup.java
+++ webapp-plus-web/src/main/java/com/chenjiacheng/webapp/startup/ApplicationStartup.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by chenjiacheng on 2024/12/19 01:02
@@ -20,8 +24,24 @@ public class ApplicationStartup implements ApplicationRunner {
     @Autowired
     private ApplicationContext ctx;
 
+    @Autowired
+    private ThreadPoolTaskExecutor customExecutor;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("ApplicationContext:{}", ctx);
+
+        // customExecutor.execute(this::clock);
+    }
+
+    private void clock() {
+        while (true) {
+            log.info("Current time:{}", LocalDateTime.now());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                log.error("Thread sleep error", e);
+            }
+        }
     }
 }
